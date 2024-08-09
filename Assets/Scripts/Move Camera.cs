f9bb6342ba8292b8dc397e12f0d1d7ada38b88bc
donghoon0329 <ghingongdp@gmail.com>
2024-08-09 17:30:14 +0900

    카메라 및 코드 정리

--- Assets/Scripts/Move Camera.cs
+++ Assets/Scripts/Move Camera.cs
@@ -5,14 +5,36 @@ using UnityEngine;
 
 public class CameraCtrl : MonoBehaviour
 {
-    public GameObject A;  //A라는 GameObject변수 선언
-    Transform AT;
+    public Transform target;  
+    public float speed;
+
+    public Vector2 center;
+    public Vector2 size;
+    float height;
+    float width;
+
     void Start()
     {
-        AT = A.transform;
+        height = Camera.main.orthographicSize;
+        width = height * Screen.width / Screen.height;
     }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(center, size);
+    }
+
     void LateUpdate()
     {
-        transform.position = new Vector3(AT.position.x, AT.position.y, transform.position.z);
+        //transform.position = new Vector3(AT.position.x, AT.position.y, transform.position.z);
+        transform.position = Vector3 .Lerp(transform.position, target.position, Time.deltaTime * speed);
+        //transform.position = new Vector3(transform.position.x, transform.position.y , -10f);
+        float Ix = size.x * 0.5f - width;
+        float clampX = Mathf.Clamp(transform.position.x , -Ix + center.x , Ix + center.x);
+
+        float Iy = size.x * 0.5f - height;
+        float clampY = Mathf.Clamp(transform.position.y, -Iy + center.y, Iy + center.y);
+
+        transform.position = new Vector3(clampX, clampY, -10f);
     }
 }
