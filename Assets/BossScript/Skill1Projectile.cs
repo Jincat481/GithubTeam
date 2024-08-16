47ecede04c05826da756182e1bc60959ddbb9b51
Jincat481 <wjswlsdh159@gmail.com>
2024-08-16 15:54:52 +0900

    풀 매니저 수정 및 스킬 3 버그 수정

--- Assets/BossScript/Skill1Projectile.cs
+++ Assets/BossScript/Skill1Projectile.cs
@@ -9,7 +9,6 @@ public class Skill1Projectile : MonoBehaviour
     public GameObject Player;
     Transform PlayerTransform;
     public float Speed;
-    public float Damage;
     Vector2 direction;
     // Start is called before the first frame update
     void Start()
@@ -25,17 +24,6 @@ public class Skill1Projectile : MonoBehaviour
 
     public void SetToPlayerPosition()
     {
-        PlayerTransform = Player.transform;
-        direction = PlayerTransform.position - transform.position;
 
-        // 플레이어에게 날아가는 속도 수정
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = direction.normalized * Speed;
-
-        // 각도 계산
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        // 각도 설정
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
